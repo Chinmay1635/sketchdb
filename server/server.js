f5ef807cc42318fbd1899ecea3849566aa32e992
Chinmay1635 <chinmaykulkarni165@gmail.com>
2026-01-13 20:47:20 +0530

    added primary route

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ app.use(cors({
   credentials: true
 }));
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the SketchDB API');
+});
+
 // Body parser middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
